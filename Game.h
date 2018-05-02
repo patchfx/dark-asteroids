9d985e71c0d4db4ef1bc5a24983f033de8c01d45
Richard Patching <richard@justaddpixels.com>
2018-05-02 17:51:00 +0100

    Move code into a game class

---
+++ Game.h
@@ -0,0 +1,18 @@
+//
+// Created by richard on 5/2/18.
+//
+
+#ifndef DARK_ASTEROIDS_GAME_H
+#define DARK_ASTEROIDS_GAME_H
+
+
+class Game {
+public:
+    Game();
+    int run();
+private:
+    bool playing;
+};
+
+
+#endif //DARK_ASTEROIDS_GAME_H
